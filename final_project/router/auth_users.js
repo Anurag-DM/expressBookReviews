ba88e8595f4ed995b88adb839376f04006462fc9
Anurag-DM <anuragadm78@gmail.com>
2026-06-16 17:34:17 -0400

    Completed Task 9 - Delete review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -68,6 +68,20 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   });
 });
 
+// Delete book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+
+  const isbn = req.params.isbn;
+  const username = req.session.authorization.username;
+
+  if (books[isbn].reviews[username]) {
+    delete books[isbn].reviews[username];
+  }
+
+  return res.status(200).json({
+    message: "Review successfully deleted"
+  });
+});
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
