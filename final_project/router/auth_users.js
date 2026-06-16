a992086745d54930ece5180d2a860e7d8905a5e4
Anurag-DM <anuragadm78@gmail.com>
2026-06-16 17:26:19 -0400

    Completed Task 8 - Add or modify review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,8 +5,11 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+  let userswithsamename = users.filter(
+    (user) => user.username === username
+  );
+  return userswithsamename.length > 0;
 }
 
 const authenticatedUser = (username, password) => {
@@ -53,8 +56,16 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+  const username = req.session.authorization.username;
+
+  books[isbn].reviews[username] = review;
+
+  return res.status(200).json({
+    message: "Review successfully added/updated"
+  });
 });
 
 module.exports.authenticated = regd_users;
